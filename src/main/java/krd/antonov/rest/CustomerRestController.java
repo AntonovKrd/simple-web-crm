906bd3c4af675bafb6b943bcbadb10b48e02de0e
Vladimir <v.antonov93@gmail.com>
2021-02-04 15:11:44 +0300

    1. add customer rest advice /customers/{customerID} 2. Exception handling with a message has been developed for this advice. .

--- src/main/java/krd/antonov/rest/CustomerRestController.java
+++ src/main/java/krd/antonov/rest/CustomerRestController.java
@@ -1,9 +1,11 @@
 package krd.antonov.rest;
 
 import krd.antonov.entity.Customer;
+import krd.antonov.rest.exception.CustomerNotFoundException;
 import krd.antonov.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,5 +25,13 @@ public class CustomerRestController {
     @GetMapping("/customers")
     public List<Customer> getCustomers() {
         return customerService.getCustomers();
+    }
+
+    @GetMapping("/customers/{customerID}")
+    public Customer getCustomer(@PathVariable long customerID) {
+        Customer customer = customerService.getCustomer(customerID);
+        if (customer == null)
+            throw new CustomerNotFoundException("Customer id not found - " + customerID);
+        return customer;
     }
 }
